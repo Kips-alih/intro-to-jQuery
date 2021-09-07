070e34824ddeee984246c825677ddb34998f917f
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-07 16:13:42 +0300

    Modify the class selectors to id selectors

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
   jQuery(document).ready(function() {
     jQuery('.clickable').click(function() {
-      $(".walrus-showing").toggle();
-      $(".walrus-hidden").toggle();
+      $("#walrus-showing").toggle();
+      $("#walrus-hidden").toggle();
     });
   });
   
